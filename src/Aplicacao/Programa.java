c4188849656a5689f1b0a60c5aeed96879a8102f
raremen <rafael_fox1@hotmail.com>
2018-08-07 09:24:59 -0300

    Cria e fecha arquivo manual

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,33 +1,53 @@
 package Aplicacao;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 public class Programa {
 
-	public static void main(String[] args) {
-		
-		File arquivo = new File("C:\\Users\\rafael.souza\\Desktop\\"
-				+ "eclipse-jee-photon-R-win32-x86_64\\ProjetosDoCurso\\Manipulando_Arquivos\\Entrada.txt");
-		Scanner sc = null;
-		
-		try {
-			
-			sc = new Scanner(arquivo);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
 
-			
-		} catch (IOException e ) {
-			System.out.println("Não encontrado o local do Arquivo Entrada.txt" + e.getMessage());
-		} finally {
-			if (sc != null) {
-			sc.close();
+	public static void main(String[] args) {
+// abertura e fechamento manual dos arquivos
+		String sCaminho = "C:\\Users\\rafael.souza\\Desktop\\"
+				+ "eclipse-jee-photon-R-win32-x86_64\\ProjetosDoCurso\\Manipulando_Arquivos\\Entrada.txt";
+
+		FileReader lerArquivo = null;
+		BufferedReader lerEscrita = null; // acelera leitura de arquivos
+
+		try {
+			lerEscrita = new BufferedReader(new FileReader(sCaminho));
+//			lerArquivo = new FileReader(sCaminho);
+//			lerEscrita = new BufferedReader(lerArquivo);
+
+			String sLinha = lerEscrita.readLine();
+
+			while (sLinha != null) {
+				System.out.println(sLinha);
+				sLinha = lerEscrita.readLine();
 			}
+		} catch (IOException e) {
+			System.out.println("Arquivo não encontrado: \n" + e.getMessage() + " Erro que aconteceu");
 		}
 
+		finally {
+			
+			try {
+				
+				if (lerEscrita != null) {
+					lerEscrita.close();
+				}
+				if (lerArquivo != null) {
+					lerArquivo.close();
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
-
 }
